1401e7e76f26b12b573c7d530063b04cd8c4927e
dumpserver <dumpserver@hotmail.com>
2022-02-03 22:46:29 -0300

    Send message to DisplayMessageActivity.

--- app/src/main/java/br/edu/ifpe/tads/pdm/pratica01/MainActivity.java
+++ app/src/main/java/br/edu/ifpe/tads/pdm/pratica01/MainActivity.java
@@ -1,13 +1,27 @@
 package br.edu.ifpe.tads.pdm.pratica01;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "Pratica01.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View object) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
